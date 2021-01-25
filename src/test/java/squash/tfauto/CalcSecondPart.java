06566669962e78bb42d4c37d27a5e75d7e50de5d
UKevin94 <kevinung94@gmail.com>
2021-01-25 10:29:38 +0100

    Added new tests for CUFs + updated README

--- src/test/java/squash/tfauto/CalcSecondPart.java
+++ src/test/java/squash/tfauto/CalcSecondPart.java
@@ -20,4 +20,25 @@ public class CalcSecondPart {
         res = Integer.parseInt(TFParamService.getInstance().getParam("DS_RES"));
         Assertions.assertTrue((var1-var2)==res, "Le résultat du calcul est incorrect.");
     }
+
+    @Test
+    @DisplayName("CheckTestCase")
+    public void checkTCCUF(){
+        var1 = Integer.parseInt(TFParamService.getInstance().getParam("CUF_TC_number"));
+        var2 = 2;
+        res = 5;
+        Assertions.assertTrue((var1-var2)==res, "Le résultat du calcul est incorrect.");
+    }
+
+    @Test
+    @DisplayName("CheckPremiumCUF")
+    public void checkPremiumCUF(){
+        var1 = Integer.parseInt(TFParamService.getInstance().getParam("CUF_TC_number"));
+        var2 = Integer.parseInt(TFParamService.getInstance().getParam("CUF_CPG_number2"));
+        var3 = Integer.parseInt(TFParamService.getInstance().getParam("CUF_IT_number3"));
+        var4 = Integer.parseInt(TFParamService.getInstance().getParam("CUF_TS_number4"));
+        var5 = Integer.parseInt(TFParamService.getInstance().getParam("DSNAME"));
+        res = Integer.parseInt(TFParamService.getInstance().getParam("DS_expectedResult"));
+        Assertions.assertTrue((var1+var2+var3+var4+var5)==res, "Le résultat du calcul est incorrect.");
+    }
 }
