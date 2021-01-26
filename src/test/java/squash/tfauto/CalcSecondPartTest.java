8d923b7aaaa514a99eddbf689ed3920fc64daddc
UKevin94 <kevinung94@gmail.com>
2021-01-26 17:05:51 +0100

    Fixed wrong CUF name ........

--- src/test/java/squash/tfauto/CalcSecondPartTest.java
+++ src/test/java/squash/tfauto/CalcSecondPartTest.java
@@ -27,7 +27,7 @@ public class CalcSecondPartTest {
     @Test
     @DisplayName("CheckTestCase")
     public void checkTCCUF(){
-        var1 = Integer.parseInt(TFParamService.getInstance().getParam("CUF_TC_number"));
+        var1 = Integer.parseInt(TFParamService.getInstance().getParam("TC_CUF_number"));
         var2 = 2;
         res = 5;
         Assertions.assertTrue((var1-var2)==res, "Le résultat du calcul est incorrect.");
@@ -36,10 +36,10 @@ public class CalcSecondPartTest {
     @Test
     @DisplayName("CheckPremiumCUF")
     public void checkPremiumCUF(){
-        var1 = Integer.parseInt(TFParamService.getInstance().getParam("CUF_TC_number"));
-        var2 = Integer.parseInt(TFParamService.getInstance().getParam("CUF_CPG_number2"));
-        var3 = Integer.parseInt(TFParamService.getInstance().getParam("CUF_IT_number3"));
-        var4 = Integer.parseInt(TFParamService.getInstance().getParam("CUF_TS_number4"));
+        var1 = Integer.parseInt(TFParamService.getInstance().getParam("TC_CUF_number"));
+        var2 = Integer.parseInt(TFParamService.getInstance().getParam("CPG_CUF_number2"));
+        var3 = Integer.parseInt(TFParamService.getInstance().getParam("IT_CUF_number3"));
+        var4 = Integer.parseInt(TFParamService.getInstance().getParam("TS_CUF_number4"));
         var5 = Integer.parseInt(TFParamService.getInstance().getParam("DSNAME"));
         res = Integer.parseInt(TFParamService.getInstance().getParam("DS_expectedResult"));
         Assertions.assertTrue((var1+var2+var3+var4+var5)==res, "Le résultat du calcul est incorrect.");
